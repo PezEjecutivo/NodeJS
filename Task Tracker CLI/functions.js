80952b7690db98f443e0e49127d0a3aca37ad52c
Pablo Valladares González <PValladaresG@proton.me>
2026-01-30 16:07:55 +0100

    add and update function fix to work in a single file

--- Task Tracker CLI/functions.js
+++ Task Tracker CLI/functions.js
@@ -1,3 +1,4 @@
+import { existsSync } from "node:fs";
 import { writeFile, readFile, readdir } from "node:fs/promises";
 
 async function getNewId() {
@@ -10,29 +11,41 @@ async function getNewId() {
 
 export async function createTask(desc) {
     const id = await getNewId();
+    let file = [];
 
-    const task = [{
+    const task = {
         id: id,
         description: desc,
         status: "Todo",
         createdAt: new Date(),
         updatedAt: "Esta tarea no ha sido actualizada."
-    }];
+    };
 
-    await writeFile(`./Tasks/${id}.json`, JSON.stringify(task));
+    if (existsSync("./Tasks.json")) {
+        file = await readFile("./Tasks.json", "utf-8");
+
+        const jsonFile = JSON.parse(file);
+        jsonFile.push(task);
+
+        await writeFile(`./Tasks.json`, JSON.stringify(jsonFile));
+
+    } else {
+        file.push(task);
+        await writeFile(`./Tasks.json`, JSON.stringify(file));
+    }
 
     console.log(`Task added successfully (ID: ${id})`);
 }
 
 export async function updateTask(id, desc) {
-    const file = await readFile(`./Tasks/${id}.json`, "utf-8");
-
+    const file = await readFile(`./Tasks.json`, "utf-8");
     const jsonFile = JSON.parse(file);
+    const index = jsonFile.findIndex(task => task.id == id);
 
-    jsonFile[0].description = desc;
-    jsonFile[0].updatedAt = new Date();
+    jsonFile[index].description = desc;
+    jsonFile[index].updatedAt = new Date();
 
-    await writeFile(`./Tasks/${id}.json`, JSON.stringify(jsonFile));
+    await writeFile(`./Tasks.json`, JSON.stringify(jsonFile));
 
     console.log(`Task updated successfully (ID: ${id})`);
 }
